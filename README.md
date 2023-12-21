424a93a95036c28f88ac8158c77c1ce839d4086b
ADNANLH <adnanharrak5@gmail.com>
2023-12-21 15:59:03 +0100

    repository

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Lab-laravel-standard
+# Lab crud standard
+## Travail à faire
+- Améliorez le projet de lab crud basic de Laravel en appliquant le design pattern Repository. Intégrez une fonction de recherche, implémentez la pagination et incluez la gestion de la table des projets.
+## Critères de validation 
+- compléter le travail de **lab crud basic**
+- Opérations CRUd pour les taches
+- Pagination
+- Recherche avec AJAX
+- Design Pattern Repositry
+- Affichage des projets
+- filtrer par projet
+- données des exemple (jeux de test)
+## Proccess d'instalation
+1. cloner lab crud basic avec la commande
+```bash
+git clone https://github.com/Jalil-Betroji/lab-crud.git
+
+```
+2. créer le fichier .env avec la commande
+```bash
+cp .env.example .env
+
+```
+3. Ajouter le nom de la base de donnée sur le fichier .env
+
+4. Migration de la base de donnée
+```bash
+php artisan migrate
+
+```
+5. Envoyer les donnée vers la base de donnée avec les seeders
+```bash
+php artisan db:seed
+
+```
+6. Executer le projet 
+```bash
+php artisan serve
+
+```
+## Référence
+- [lab-crud-standard](https://github.com/labs-web/lab-crud-laravel-standard)
--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -9,54 +9,17 @@ use Illuminate\Http\Request;
 class TasksController extends Controller
 {
     //
-    public function index(Request $request){
-        $tasks = Task::paginate(3);
-        if($request->ajax()){
-            $searchQuery = $request->get('searchValue');
-            $searchQuery = str_replace(' ', '%', $searchQuery);
-            $tasks = Task::query()->where('nom','like','%'.$searchQuery.'%')->orWhere('description' , 'like', '%' . $searchQuery . '%')->paginate(3);
-            return view('search', compact('tasks'))->render();
-        }
-        return view('home', compact('tasks'));
+    protected $tasksRepository;
+    protected $projectRepository;
+    public function __construct(TasksRepository $tasksRepository, ProjectRepository $projectRepository){
+        $this->tasksRepository = $tasksRepository;
+        $this->projectRepository = $projectRepository;
     }
+    public function show(Request $request, $id){
+        $task = $this->tasksRepository->show($id);
+        $projectData = $this->projectRepository->getData();
+    }
+   
 
-    public function create(){
-        $projects = Project::all();
-        return view('add', compact('projects'));
-    }
-
-    public function store(Request $request){
-        $task = new Task;
-        $validateData = $request->validate([
-            'nom' => 'required|max:50',
-            'projetId'=> 'required',
-            'description' => 'required'
-        ]);
-        $task::create($validateData);
-        return redirect()->route('add.task')->with('success', 'tach a été ajouter avec succés');
-    }
-
-    public function edit($id){
-        $task = Task::findOrFail($id);
-        $projects = Project::all();
-        return view('edit', compact('task', 'projects'));
-    }
-
-    public function update(Request $request, $id){
-        $task = Task::findOrFail($id);
-        $validatedData = $request->validate([
-            'nom' => 'required| max:50',
-            'projetId' => 'required',
-            'description' => 'required'
-        ]);
-        $task->update($validatedData);
-        return redirect()->route('edit.task', ['id' => $task->id])->with('success', 'La tache a été modifier avec succés');
-    }
-
-    public function delete($id){
-        $task = Task::findOrFail($id);
-        $task->delete();
-        return redirect('/');
-    }
 
 }
